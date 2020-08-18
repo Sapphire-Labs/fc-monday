a0972e983f8f8b9cb65321fd6964e204c9c4ba53
Jadon Fan <35983083+JadonFan@users.noreply.github.com>
2020-08-18 17:50:19 -0400

    Implement monday app (#1)
    * Implement monday app
    
    * Improve l10n

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,12 +1,12 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
-export default function () {
+export default function (primaryColor) {
   return createMuiTheme({
     palette: {
       type: "light",
       primary: {
-        main: "#0D47A1",
+        main: primaryColor || "#0D47A1",
         contrastText: "#FFF",
       },
       secondary: {
@@ -28,6 +28,16 @@ export default function () {
             height: "100%",
             margin: 0,
           },
+          main: {
+            height: "100vh",
+            width: "100vw",
+            padding: 24,
+          },
+        },
+      },
+      MuiTooltip: {
+        tooltipPlacementBottom: {
+          marginTop: "8px !important",
         },
       },
     },
